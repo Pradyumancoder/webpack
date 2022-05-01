e12251618cdd994bb22334b4ecc8ef4e12f9ce00
unknown <shuklapradyuman786@gmail.com>
2022-05-01 21:30:38 +0530

    react

---
+++ components/navbar.js
@@ -0,0 +1,23 @@
+
+function navbar (){
+    return `  
+    <h3>
+    <a href="">Home</a>
+</h3>
+<div id="option"></div>
+    <h3>
+        <a href="jewelery.html">jewelery</a>
+    </h3>
+    <h3>
+        <a href="electronic.html">electronic</a>
+    </h3>
+    <h3>
+        <a href="login.html">log in</a>
+    </h3>
+    <h3>
+        <a href="signup.html">Sign up</a>
+    </h3>
+    </div>
+    `
+}
+export {navbar}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import {navbar} from "../components/navbar";
+
+import "../styles/navbar.css";
+
+let navbar_div=document.getElementById("navbar")
+navbar_div.innerHTML=navbar();
+
